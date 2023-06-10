f4e0df0f82cd12b7dabeadf7e58861d8bf50fee2
jishan1019 <imranislamjishan200@gmail.com>
2023-06-11 00:04:42 +0600

    error page design complite

--- src/components/AllLayout/ErrorLayout/ErrorPage.jsx
+++ src/components/AllLayout/ErrorLayout/ErrorPage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>404 not found</div>;
+  return (
+    <div>
+      <img
+        className="mx-auto"
+        src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7898.jpg"
+        alt=""
+      />
+
+      <div className="text-center p-4 md:p-0 md:max-w-[35%] space-y-4 mx-auto">
+        <h1 className="text-2xl font-bold">Oppse! Somthing's missing...</h1>
+        <p className="text-lg ">
+          It seems like we donut find what you searched. The page you were
+          looking for doesn't exist, isn't available or was loading incorrectly.
+        </p>
+
+        <button className="py-2 px-4 bg_primary rounded-md">
+          <Link to="/">Back To Home</Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
