dd0fb257af4f93af8fc49be98471dcde36feba70
jishan1019 <imranislamjishan200@gmail.com>
2023-06-14 15:44:00 +0600

    instractor page data show done

--- src/components/AllLayout/InstractorLayout/Instactor/Instractor.jsx
+++ src/components/AllLayout/InstractorLayout/Instactor/Instractor.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import HelmetTitle from "../../ShareLayout/Helmet/HelmetTitel";
 import MovingText from "react-moving-text";
 import useAxiosLoadData from "../../../CustomHook/useAxiosLoadData";
+import Loader from "../../ShareLayout/Loader/Loader";
+import {
+  AiFillMail,
+  AiFillSafetyCertificate,
+  AiOutlineCheckCircle,
+  AiOutlineFork,
+} from "react-icons/ai";
 
 const Instractor = () => {
   // Axios Code Here
@@ -11,8 +18,20 @@ const Instractor = () => {
 
   console.log(data);
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Error: {error.message}
+      </div>
+    );
+  }
+
   return (
-    <div className="mb-16">
+    <section className="mb-16">
       <HelmetTitle title="Instractor" />
 
       <div className="pt-12 pb-12 space-y-2 bg_primary text-center  ">
@@ -43,7 +62,47 @@ const Instractor = () => {
           </MovingText>
         </h1>
       </div>
-    </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-10 mb-10 ml-4 mr-4">
+        {data?.map((instractor) => (
+          <div
+            key={instractor._id}
+            className="card w-full bg-gradient-to-t from-white to-[#F1F1F1] shadow-md rounded-sm"
+          >
+            <figure className=" mx-auto mt-8">
+              <img
+                className="rounded-full w-24 h-24"
+                src={instractor?.image}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title mx-auto">{instractor?.name}</h2>
+              <p className="inline-flex items-center mt-2 gap-1 justify-start ">
+                <AiFillSafetyCertificate />
+                <strong>Instractors</strong>: {instractor?.class_name}
+              </p>
+              <p className="inline-flex items-center gap-1 justify-start ">
+                <AiFillMail />
+                <strong>Email</strong>: {instractor?.email}
+              </p>
+              <p className="inline-flex items-center gap-1 justify-start ">
+                <AiOutlineFork />
+                <strong>Class</strong>: {instractor?.email}
+              </p>
+              <div className="card-actions justify-start mt-4">
+                <button
+                  className="btn bg_primary
+"
+                >
+                  See Classes
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
